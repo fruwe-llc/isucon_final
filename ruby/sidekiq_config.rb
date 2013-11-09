623dfaf04de49e0c106091792f01e462b888b1ed
cads <cdonderis@gmail.com>
2013-11-09 17:46:11 +0900

    added config

--- ruby/sidekiq_config.rb
+++ ruby/sidekiq_config.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379', :namespace => 'isucon' }
+  config.redis = { :url => 'redis://ifp2:6379', :namespace => 'isucon' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379', :namespace => 'isucon' }
+  config.redis = { :url => 'redis://ifp2:6379', :namespace => 'isucon' }
 end
