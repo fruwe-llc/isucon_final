5e2d9222d30c848c8a7a5b01552988b06551d40f
cads <cdonderis@gmail.com>
2013-11-09 15:30:06 +0900

    sidekiq skeleton

---
+++ ruby/isuworker.rb
@@ -0,0 +1,16 @@
+require 'sidekiq'
+require 'sidekiq_config'
+
+
+# Run Sidekiq with
+# bundle exec sidekiq -C sidekiq.yml -r ./isuworker.rb
+class Isuworker
+  include Sidekiq::Worker
+
+  def perform
+    #Do Stuff with worker
+    1.upto(100) do |index|
+      puts index
+    end
+  end
+end
